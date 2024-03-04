9099907761fceba3d31102848a6451a14a9c1824
Dragonf-j <55980623+Dragonf-j@users.noreply.github.com>
2024-03-04 22:56:54 +0100

    Cambios ne la pagina create

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PagesLestview, PageDetailView
+from .views import PagesLestview, PageDetailView, PageCreateView
 
-urlpatterns = [
+pages_patterns = ([
     path('', PagesLestview.as_view(), name='pages'),
     path('<int:pk>/<slug:slug>/', PageDetailView.as_view(), name='page'),
-]
+    path('create/', PageCreateView.as_view(), name='create'),
+    ], 'pages')
--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,8 @@
 from .models import Page
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse, reverse_lazy
 
 # Create your views here.
 class PagesLestview(ListView):
@@ -9,3 +11,10 @@ class PagesLestview(ListView):
 
 class PageDetailView(DetailView):
     model = Page
+
+class PageCreateView(CreateView):
+    model = Page
+    fields = ['title','content','order']
+    success_url = reverse_lazy('pages:pages')
+    #def get_success_url(self) -> str:
+        #return reverse('pages:pages')
