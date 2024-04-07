80fdb59d9729b0ef4e2353c549474c18cbde9ea8
Juan Antonio <jmjejuan@gmail.com>
2024-04-07 11:48:18 +0200

    update view

--- pages/views.py
+++ pages/views.py
@@ -2,8 +2,9 @@
 from .models import Page
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
+
 
 # Create your views here.
 class PagesLestview(ListView):
@@ -18,3 +19,11 @@ class PageCreateView(CreateView):
     success_url = reverse_lazy('pages:pages')
     #def get_success_url(self) -> str:
         #return reverse('pages:pages')
+
+class PageUpdateView(UpdateView):
+    model = Page
+    fields = ['title','content','order']
+    template_name_suffix = "_update_form"
+    success_url = reverse_lazy('pages:pages')
+    def get_success_url(self):
+        return reverse_lazy('pages:update', args={self.object.id}) + '?ok'
