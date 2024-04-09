0306d0a00e2edade975c17d1830dfde0ab1ff4f3
Juan Antonio <jmjejuan@gmail.com>
2024-04-09 22:57:35 +0200

    mixin para las vistas

--- pages/views.py
+++ pages/views.py
@@ -5,7 +5,17 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
 from .forms import PageForm
+from django.shortcuts import redirect
 
+
+class stafRequeredMixin(object):
+    """
+    Este mixin sirve para que el usuario sea miembro del staff y pueda editar, crear o borrar paginas siempre y cuando este logueado y sea miebro
+    """
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_staff:
+            return redirect(reverse_lazy('admin:login'))
+        return super(stafRequeredMixin, self).dispatch(request, *args, **kwargs)
 
 # Create your views here.
 class PagesLestview(ListView):
@@ -14,15 +24,16 @@ class PagesLestview(ListView):
 class PageDetailView(DetailView):
     model = Page
 
-class PageCreateView(CreateView):
+class PageCreateView(stafRequeredMixin, CreateView):
     model = Page
     form_class = PageForm
     #fields = ['title','content','order']
     success_url = reverse_lazy('pages:pages')
     #def get_success_url(self) -> str:
         #return reverse('pages:pages')
+    
 
-class PageUpdateView(UpdateView):
+class PageUpdateView(stafRequeredMixin, UpdateView):
     model = Page
     form_class = PageForm
     template_name_suffix = "_update_form"
@@ -30,6 +41,6 @@ class PageUpdateView(UpdateView):
     def get_success_url(self):
         return reverse_lazy('pages:update', args={self.object.id}) + '?ok'
 
-class PageDelete(DeleteView):
+class PageDelete(stafRequeredMixin, DeleteView):
     model = Page
     success_url = reverse_lazy('pages:pages')
